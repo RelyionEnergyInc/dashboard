eeaff550bc818a13a978a37fc73dba4ebbdbe76d
Jack Davey <johncdavey2000@gmail.com>
2022-09-30 16:31:33 -0700

    Display layout updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,17 @@ import ListDataSamples from './components/listDataSamples';
 
 function App() {
   return (
-    <>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      height: "100vh",
+      width: "100vw"
+
+    }}>
       <ListDataSamples />
-    </>
+    </div>
   );
 }
 
